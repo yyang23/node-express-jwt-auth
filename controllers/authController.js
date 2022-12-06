de5fd12fc384625d71483ae989f9a975fe8e6f5d
yyang23 <yyang24@ivytech.edu>
2022-12-06 18:27:47 -0500

    Testing routes and post req

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,10 +7,14 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = (req, res) => {
-   res.send('new signup')
+    const { email, password } = req.body;
+    console.log(email, password);
+    res.send('sign up');
 }
 
 module.exports.login_post = (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
     res.send('user login');
 }
 
