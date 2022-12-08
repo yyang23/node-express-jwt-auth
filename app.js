5786c737dfb1069cdba47dc43542f4675a572874
yyang23 <yyang24@ivytech.edu>
2022-12-07 20:51:28 -0500

    Cookies Primer

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookiePraser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookiePraser());  // access a cookie method
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,3 +24,22 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use('/', authRoutes);
+
+// # 9 cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('set-cookie', 'newUser=true')   // how to identify the cookie. set a cookie
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookies')
+
+});
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;  // cookies is javascript object, can use dot notation to get different cookies
+  console.log(cookies);
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+});
